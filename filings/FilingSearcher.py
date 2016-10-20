f3a3662bdd50c20b10470574692304727601b7d0
dado0583 <davidjdoherty@gmail.com>
2016-10-19 22:05:49 -0400

    Completing docker container to run the Filing Finder

--- filings/FilingSearcher.py
+++ filings/FilingSearcher.py
@@ -4,10 +4,18 @@ Created on Sep 18, 2016
 @author: dave
 '''
 import concurrent.futures
+import os
+import sys
 import zlib
 
-
+try:
+    sys.path.index(os.getcwd()) # Or os.getcwd() for this directory
+except ValueError:
+    sys.path.append(os.getcwd()) # Or os.getcwd() for this directory
+    
 from sec_config.Coverage import SECCoverage
+from sec_config.SearchTerms import SearchTerms
+from config.MongoDb import MongoDb
 
 
 class FilingSearcher(object):
@@ -22,29 +30,34 @@ class FilingSearcher(object):
         #pool.submit(outputRaceResults, url)  
             
     def searchFilings(self):   
-        with concurrent.futures.ThreadPoolExecutor(max_workers=10) as pool:             
+        with concurrent.futures.ThreadPoolExecutor(max_workers=1) as pool:             
             for cik in self.ciks:
                 pool.submit(self.searchFiling, self.searchTerms, cik)  
                 #self.searchFiling(cik)
                         
     def searchFiling(self, searchTerms, cik):
-        print('Searching filings for CIK:{}'.format(str(cik)))                
-        filingsCollection = MongoDb().getDb('sec')['filings']
-        searchCollection = MongoDb().getDb('sec')['searchresults']
+        try:
+            print('Searching filings for CIK:{}'.format(str(cik)))                
+            filingsCollection = MongoDb().getDb('sec')['filings']
+            searchCollection = MongoDb().getDb('sec')['searchresults']
+            
+            collection = filingsCollection.find({"cik":cik}, {"RawText":1, "_id":1})
+            
+            for item in collection:            
+                text = zlib.decompress(item['RawText']).decode("utf-8")
+                    
+                matches = self.findMatches(text, searchTerms)
+                if len(matches)>0:
+                    print("Matches for {} (ID={}) are {}".format(cik, item['_id'], matches))
+                    resultsData = {}
+                    resultsData["_id"] = item['_id']                
+                    resultsData["cik"] = cik
+                    resultsData["matches"] = matches
+                    searchCollection.save(resultsData)
         
-        collection = filingsCollection.find({"cik":cik}, {"RawText":1, "_id":1})
-        
-        for item in collection:            
-            text = zlib.decompress(item['RawText']).decode("utf-8")
-                
-            matches = self.findMatches(text, searchTerms)
-            if len(matches)>0:
-                print("Matches for {} (ID={}) are {}".format(cik, item['_id'], matches))
-                resultsData = {}
-                resultsData["_id"] = item['_id']                
-                resultsData["cik"] = cik
-                resultsData["matches"] = matches
-                searchCollection.save(resultsData)
+        except Exception as e:
+            logging.exception(e)
+            print(str(e))
 
   
     def findMatches(self, item, searchTerms):
